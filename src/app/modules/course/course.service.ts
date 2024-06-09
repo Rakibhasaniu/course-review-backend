aafbd89fe396fae3f56e52f502aaef8ca73c187a
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-06-09 15:54:03 +0600

    QueryBuilder implemented

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,3 +1,4 @@
+import QueryBuilder from "../../builder/QueryBuilder";
 import { TCategory } from "../category/category.interface";
 import { TCourse } from "./course.interface";
 import Course from "./course.model";
@@ -22,31 +23,49 @@ const createCourseIntoDB = async(payload:TCourse) => {
 }
 const getAllCourseFromDB  = async(query:Record<string ,unknown>) => {
     // console.log(query)
-    const queryObj={...query}
-    let searchTerm ='';
-    if(query?.searchTerm){
-        searchTerm=query?.searchTerm as string;
-    }
-    const searchQuery = Course.find({
-        $or:CourseSearchAbleFields.map((field)=>({
-            [field]:{$regex:searchTerm, $options:'i'}
-    }))    
-    });
-    const excludeFields =['searchTerm','sort','limit']
-    excludeFields.forEach((el)=>delete queryObj[el] )
-    const filterQuery =  searchQuery.find(queryObj)
-    let sort='-price';
-    if(query?.sort){
-        sort=query?.sort as string;
-    }
-    const sortQuery =  filterQuery.sort(sort)
-    let limit=5;
-    if(query?.limit){
-        limit=query?.limit as  number;
-    }
+    // const queryObj={...query}
+    // let searchTerm ='';
+    // if(query?.searchTerm){
+    //     searchTerm=query?.searchTerm as string;
+    // }
+    // const searchQuery = Course.find({
+    //     $or:CourseSearchAbleFields.map((field)=>({
+    //         [field]:{$regex:searchTerm, $options:'i'}
+    // }))    
+    // });
+    // const excludeFields =['searchTerm','sort','limit','page','fields']
+    // excludeFields.forEach((el)=>delete queryObj[el] )
+    // const filterQuery =  searchQuery.find(queryObj)
+    // let sort='-price';
+    // if(query?.sort){
+    //     sort=query?.sort as string;
+    // }
+    // const sortQuery =  filterQuery.sort(sort)
+    // let page=1;
+    // let limit=5;
+    // let skip=0;
+    // if(query?.limit){
+    //     limit=Number(query?.limit);
+    
+    // }
 
-    const limitQuery = await sortQuery.limit(limit);
-    return limitQuery ;
+    // if(query?.page){
+    //     page=Number(query?.page)
+    //     skip=(page-1)*limit;
+    // }
+  
+    // const paginateQuery =  sortQuery.skip(skip)
+    // const limitQuery =  paginateQuery.limit(limit);
+    // let fields='-__v';
+    // if(query?.fields){
+    //     fields=(query?.fields as string).split( ',').join(' ');
+
+    // }
+    // const fieldQuery = await limitQuery.select(fields);
+    // return fieldQuery ;
+    const studentQuery = new QueryBuilder(Course.find(),query).search(CourseSearchAbleFields).filter().sort().paginate().fields();
+    const result = await studentQuery.modelQuery;
+    return result;
 }
 
 export const CourseServices = {
